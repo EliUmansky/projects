50092d2d00330f221023088270afa3988f188d95
eli.umansky <umansky.eli@gmail.com>
2020-01-27 13:58:02 +0200

    search2

--- projects/utils/search.c
+++ projects/utils/search.c
@@ -5,7 +5,7 @@
 #define FOUND (0)
 #define NOT_FOUND (1)
 
-static size_t FindJump(size_t nmemb, size_t root) ;
+static int FindJump(size_t nmemb, size_t root) ;
 
 void *BSearch(const void *base, size_t nmemb, size_t size, 
 									cmp_func_t cmp_func, const void *key)
@@ -34,8 +34,8 @@ void *BSearch(const void *base, size_t nmemb, size_t size,
 void *RecBSearch(const void *base, size_t nmemb, size_t size, 
 									cmp_func_t cmp_func, const void *key)
 {
-	size_t next_jump = nmemb / 2 * size;
-	char *searcher = (char*)base + next_jump;
+	size_t next_jump = (nmemb / 2) * size;
+	char *searcher = (char*)base + next_jump, *end = (char*)base + (nmemb - 1) * size;
 	int cmp_value = NOT_FOUND;
 
 	if (0 == nmemb)
@@ -50,12 +50,13 @@ void *RecBSearch(const void *base, size_t nmemb, size_t size,
 	}
 	else if (0 < cmp_value)
 	{
-	
-		return RecBSearch(searcher + size, nmemb / 2, size, cmp_func, key);
+		return RecBSearch(searcher + size, ((size_t)(end - searcher)) / size, 
+						  size, cmp_func, key);
 	}
 	else
 	{
-		return RecBSearch(base, nmemb / 2, size, cmp_func, key);
+		return RecBSearch(base, ((size_t)(searcher - (char*)base)) / size,
+						 size, cmp_func, key);
 	}
 }
 
@@ -64,12 +65,12 @@ void *JumpSearch(const void *base, size_t nmemb, size_t size,
 {
 	char *jumper = (void*)base, *end = (char*)base + (nmemb -1) * size;
 	size_t root = (size_t)sqrt(nmemb);
-	size_t jump = FindJump(nmemb, root);
+	int jump = FindJump(nmemb, root);
 	int cmp_value = NOT_FOUND;
 
 	jumper += jump * size;
 
-	while (jumper <= end && 0 < (cmp_value = cmp_func(key, jumper)))
+	while (jumper <= end && 0 <= jump && 0 < (cmp_value = cmp_func(key, jumper)))
 	{
 		jumper += jump * size;
 		--jump;
@@ -93,9 +94,10 @@ void *JumpSearch(const void *base, size_t nmemb, size_t size,
 	return (0 == cmp_func(key, jumper)) ? jumper : NULL;
 }
 
-static size_t FindJump(size_t nmemb, size_t root) 
+static int FindJump(size_t nmemb, size_t root) 
 { 
-	size_t jump = root, arr_len = jump * (jump + 1) / 2; 
+	int jump = root;
+	size_t arr_len = jump * (jump + 1) / 2; 
 
     if (0 == nmemb || 1 == nmemb)
 	{ 
--- projects/utils/searchtest.c
+++ projects/utils/searchtest.c
@@ -23,59 +23,64 @@ void BSearchTest()
 {
 	int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	int arr2[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};	
-	int num = 6, num2 = 9, num3 = 10, num4 = 11;
+	int i = 0, num = 11, num2 = 0;
 
 	puts("\nBinary search");
 	puts("-------------");
-	test(6, *(int*)BSearch(arr, 10, sizeof(int), &CompareFunc, &num),
-	"Looking for existing element");
-	test(9, *(int*)BSearch(arr, 10, sizeof(int), &CompareFunc, &num2),
-	"Looking for existing element");
-	test(10, *(int*)BSearch(arr, 10, sizeof(int), &CompareFunc, &num3),
-	"Looking for existing element");
-	addresstest(NULL, BSearch(arr, 10, sizeof(int), &CompareFunc, &num4),
+
+		
+	for (i = 0; i < 10; ++i)
+	{
+		test(i + 1, *(int*)BSearch(arr, 10, sizeof(int), &CompareFunc, arr + i),
+		"Looking for existing element");
+	}
+	addresstest(NULL, BSearch(arr, 10, sizeof(int), &CompareFunc, &num),
+	"Looking for non-existing element");
+	addresstest(NULL, BSearch(arr, 10, sizeof(int), &CompareFunc, &num2),
 	"Looking for non-existing element\n");
 
-	test(6, *(int*)BSearch(arr2, 11, sizeof(int), &CompareFunc, &num),
-	"Looking for existing element");
-	test(9, *(int*)BSearch(arr2, 11, sizeof(int), &CompareFunc, &num2),
-	"Looking for existing element");
-	test(10, *(int*)BSearch(arr2, 11, sizeof(int), &CompareFunc, &num3),
-	"Looking for existing element");
-	test(11, *(int*)BSearch(arr2, 11, sizeof(int), &CompareFunc, &num4),
-	"Looking for existing element");
+	for (i = 0; i < 11; ++i)
+	{
+		test(i + 1, *(int*)BSearch(arr2, 11, sizeof(int), &CompareFunc, arr2 + i),
+		"Looking for existing element");
+	}
+	addresstest(NULL, BSearch(arr2, 11, sizeof(int), &CompareFunc, &num2),
+	"Looking for non-existing element\n");
 }
 void RecBSearchTest()
 {
 	int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	int arr2[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};	
-	int num = 6, num2 = 9, num3 = 10, num4 = 11;
+	int i = 0, num = 11, num2 = 0;
 
 	puts("\nRec binary search");
 	puts("-----------------");
-	test(6, *(int*)BSearch(arr, 10, sizeof(int), &CompareFunc, &num),
-	"Looking for existing element");
-	test(9, *(int*)BSearch(arr, 10, sizeof(int), &CompareFunc, &num2),
-	"Looking for existing element");
-	test(10, *(int*)BSearch(arr, 10, sizeof(int), &CompareFunc, &num3),
-	"Looking for existing element");
-	addresstest(NULL, BSearch(arr, 10, sizeof(int), &CompareFunc, &num4),
+	
+	for (i = 0; i < 10; ++i)
+	{
+		test(i + 1, *(int*)RecBSearch(arr, 10, sizeof(int), &CompareFunc, arr + i),
+		"Looking for existing element");
+	}
+
+	addresstest(NULL, RecBSearch(arr, 10, sizeof(int), &CompareFunc, &num),
+	"Looking for non-existing element");
+	addresstest(NULL, RecBSearch(arr, 10, sizeof(int), &CompareFunc, &num2),
 	"Looking for non-existing element\n");
 
-	test(6, *(int*)BSearch(arr2, 11, sizeof(int), &CompareFunc, &num),
-	"Looking for existing element");
-	test(9, *(int*)BSearch(arr2, 11, sizeof(int), &CompareFunc, &num2),
-	"Looking for existing element");
-	test(10, *(int*)BSearch(arr2, 11, sizeof(int), &CompareFunc, &num3),
-	"Looking for existing element");
-	test(11, *(int*)BSearch(arr2, 11, sizeof(int), &CompareFunc, &num4),
-	"Looking for existing element");
+	for (i = 0; i < 11; ++i)
+	{
+		test(i + 1, *(int*)RecBSearch(arr2, 11, sizeof(int), &CompareFunc, arr2 + i),
+		"Looking for existing element");
+	}
+	addresstest(NULL, RecBSearch(arr2, 11, sizeof(int), &CompareFunc, &num2),
+	"Looking for non-existing element\n");
 }
 
 void JumpSearchTest()
 {
 	int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	int arr2[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};	
+	int arr3[1] = {3};
 	int i = 0, num = 11, num2 = 0;
 
 	puts("\nJump search");
@@ -98,6 +103,12 @@ void JumpSearchTest()
 	}
 	addresstest(NULL, JumpSearch(arr2, 11, sizeof(int), &CompareFunc, &num2),
 	"Looking for non-existing element\n");
+
+	test(3, *(int*)JumpSearch(arr3, 1, sizeof(int), &CompareFunc, arr3),
+		"Looking for existing element");
+	addresstest(NULL, JumpSearch(arr3, 1, sizeof(int), &CompareFunc, &num2),
+	"Looking for non-existing element\n");
+
 }
 
 static int CompareFunc(const void *data_to_find, const void *data)
