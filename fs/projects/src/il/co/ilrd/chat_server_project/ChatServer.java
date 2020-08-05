bc092c4db1f1646874a4886619dae9ac50249189
eli.umansky <umansky.eli@gmail.com>
2020-08-05 13:30:12 +0000

    chat server

---
+++ fs/projects/src/il/co/ilrd/chat_server_project/ChatServer.java
@@ -0,0 +1,39 @@
+package il.co.ilrd.chat_server_project;
+
+public interface ChatServer {
+	/**
+	 * @param email the user's email
+	 * @param name the user's name
+	 * @param peer represents the communicating peer
+	 */
+	public void logIn(int msgId, String email, String name, PeerResponse peer);
+	
+	/**
+	 * @param userId 
+	 * @param groupName name of the group to be created.
+	 */
+	void createNewGroup(int msgId, Integer userId, String groupName);
+	
+	/**
+	 * @param userId
+	 * @param groupId a unique id that each group has, given at creation.
+	 */
+	public void joinGroup(int msgId, Integer userId, String groupName);
+	
+	/**
+	 * @param userId
+	 * @param groupId a unique id that each group has, given at creation.
+	 */
+	public void leaveGroup(int msgId, Integer userId, String groupName);
+
+	/**
+	 * @param userId
+	 * @param groupId a unique id that each group has, given at creation.
+	 * @param msg the message sent by the user
+	 */
+	public void sendMsg(int msgId, Integer userId, String groupName, String msg);
+	
+	public void logOut(int msgId, Integer userId);
+
+	
+}
