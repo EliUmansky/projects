68dbf3d8b0799bb146a95dc519966246a48e314e
eli.umansky <umansky.eli@gmail.com>
2020-08-05 13:32:19 +0000

    chat server

---
+++ fs/projects/src/il/co/ilrd/chat_server_project/UsrProperties.java
@@ -0,0 +1,8 @@
+package il.co.ilrd.chat_server_project;
+
+import java.awt.Color;
+import java.io.Serializable;
+
+public interface UsrProperties extends Serializable {
+	public Color getColor();
+}
